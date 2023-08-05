a0a162289edc2f4e51c0d0d4d764a17740d69689
Brahim HM <brahimhm98@gmail.com>
2023-08-05 23:04:21 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards=(arr)=>{
+    let result=[]
+    for(let i=0;i<arr.length;i++){
+        result.push("Thank you, "+arr[i]+", for the wonderful surprise gift!")
+    }
+    return result
+}
+
+const countDown=(num)=>{
+    while(num>=0){
+        console.log(num)
+        num--
+    }
+}
